163508a14f24f3ed49a0da6ad6fb01823a996162
Shristi6054 <shristimahato6054@gmail.com>
2025-05-10 23:45:07 +0545

    Details of content added

--- nav/src/App.js
+++ nav/src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Routes ,Route } from 'react-router-dom';
+import { FaFacebook } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaYoutube } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div className='Nav-header'>
+          <div className='App-logo'>
+            <h1>Logo</h1>
+          </div>
+          <nav className='Navbar'>
+            <ul className='nav-links'>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/gallery">Gallery</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </nav>
+        </div>
+        
+        <Routes>
+          <Route exact path='/' />
+          <Route path='/about' />
+          <Route path='/gallery' />
+          <Route path='/contact' />
+        </Routes>
+
+        <div className='footer'>
+          <div className='address'>
+            <h1>Contact us</h1>
+            <p>Email : shristimahato6054@gmail.com
+            <br/>Phone : +977 9817538939
+            <br/>Address : Kathmandu, putalisadak</p>
+          </div>
+          <div className='about'>
+            <h1>About Us</h1>
+            <p>We are a team dedicated to providing the best products <br/>and services to our customers.</p>
+          </div>
+          <div className='quick-links'>
+            <h1>Quick Links</h1>
+            <ul className='links'>
+              <li><Link to ='/'>Home</Link></li>
+              <li><Link to ='/about'>About</Link></li>
+              <li><Link to = '/gallery'>Gallery</Link></li>
+              <li><Link to = '/contact'>Contact</Link></li>
+            </ul>
+          </div>
+          <div className='social-icons'>
+            <h1>Social Platforms</h1>
+            <div className='foot-icons'>
+            <div className='facebook'>
+              <FaFacebook/>
+            </div>
+            <div className='twitter'>
+              <FaTwitter/>
+            </div>
+            <div className='youtube'>
+              <FaYoutube/>
+            </div>
+            <div className='insta'>
+              <FaInstagram/>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
